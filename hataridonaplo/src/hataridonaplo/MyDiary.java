7f708502423ace67e876729b073102333e5c31ee
mnagy95 <madeon95@gmail.com>
2017-04-02 20:23:10 +0200

    Add files via upload

--- hataridonaplo/src/hataridonaplo/MyDiary.java
+++ hataridonaplo/src/hataridonaplo/MyDiary.java
@@ -13,13 +13,16 @@ public class MyDiary{
 	static JLabel labelofMonth, labelofYear;
 	static JButton previousButton, nextButton;
 	static JTable tableCalendar;
-	static JComboBox comboBoxforChoosingYear;
+	static JComboBox<String> comboBoxforChoosingYear;
 	static JFrame mainFrame;
 	static Container Pane;
 	static DefaultTableModel modellofTableCalendar; //Table model
-	static JScrollPane stblCalendar; //The scrollpane
-	static JPanel pnlCalendar;
+	static JScrollPane scrollTableCalendar; //The scrollpane
+	static JPanel panelCalendar;
 	static int realYear, realMonth, realDay, currentYear, currentMonth;
+	
+	static JLabel newEvent, yearOfEvent, monthOfEvent, dayOfEvent;
+	static Choice choiceOfYear, choiceOfMonth, choiceOfDay;
 
 	public static void main (String args[]){
 		//Look and feel
@@ -30,8 +33,8 @@ public class MyDiary{
 		catch (UnsupportedLookAndFeelException e) {}
 
 		//Prepare frame
-		mainFrame = new JFrame ("Gestionnaire de clients"); //Create frame
-		mainFrame.setSize(330, 375); //Set size to 400x400 pixels
+		mainFrame = new JFrame ("My Diary"); //Create frame
+		mainFrame.setSize(600, 400); //Set size 
 		Pane = mainFrame.getContentPane(); //Get content pane
 		Pane.setLayout(null); //Apply null layout
 		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Close when X is clicked
@@ -39,54 +42,54 @@ public class MyDiary{
 		//Create controls
 		labelofMonth = new JLabel ("January");
 		labelofYear = new JLabel ("Change year:");
-		comboBoxforChoosingYear = new JComboBox();
+		comboBoxforChoosingYear = new JComboBox<>();
 		previousButton = new JButton ("<<");
 		nextButton = new JButton (">>");
 		modellofTableCalendar = new DefaultTableModel(){public boolean isCellEditable(int rowIndex, int mColIndex){return false;}};
 		tableCalendar = new JTable(modellofTableCalendar);
-		stblCalendar = new JScrollPane(tableCalendar);
-		pnlCalendar = new JPanel(null);
+		scrollTableCalendar = new JScrollPane(tableCalendar);
+		panelCalendar = new JPanel(null);
 
 		//Set border
-		pnlCalendar.setBorder(BorderFactory.createTitledBorder("Calendar"));
+		panelCalendar.setBorder(BorderFactory.createTitledBorder("Calendar"));
 		
 		//Register action listeners
-		previousButton.addActionListener(new btnPrev_Action());
-		nextButton.addActionListener(new btnNext_Action());
-		comboBoxforChoosingYear.addActionListener(new cmbYear_Action());
+		previousButton.addActionListener(new PreviousButtonAction());
+		nextButton.addActionListener(new NextButtonAction());
+		comboBoxforChoosingYear.addActionListener(new ComboBoxYearAction());
 		
 		//Add controls to pane
-		Pane.add(pnlCalendar);
-		pnlCalendar.add(labelofMonth);
-		pnlCalendar.add(labelofYear);
-		pnlCalendar.add(comboBoxforChoosingYear);
-		pnlCalendar.add(previousButton);
-		pnlCalendar.add(nextButton);
-		pnlCalendar.add(stblCalendar);
+		Pane.add(panelCalendar);
+		panelCalendar.add(labelofMonth);
+		panelCalendar.add(labelofYear);
+		panelCalendar.add(comboBoxforChoosingYear);
+		panelCalendar.add(previousButton);
+		panelCalendar.add(nextButton);
+		panelCalendar.add(scrollTableCalendar);
 		
 		//Set bounds
-		pnlCalendar.setBounds(0, 0, 320, 335);
-		labelofMonth.setBounds(160-labelofMonth.getPreferredSize().width/2, 25, 100, 25);
+		panelCalendar.setBounds(0, 0, 500, 335);
+		labelofMonth.setBounds(160-labelofMonth.getPreferredSize().width/2, 20, 100, 25);
 		labelofYear.setBounds(10, 305, 80, 20);
 		comboBoxforChoosingYear.setBounds(230, 305, 80, 20);
 		previousButton.setBounds(10, 25, 50, 25);
 		nextButton.setBounds(260, 25, 50, 25);
-		stblCalendar.setBounds(10, 50, 300, 250);
+		scrollTableCalendar.setBounds(10, 50, 300, 250);
 		
 		//Make frame visible
-		mainFrame.setResizable(false);
+		mainFrame.setResizable(true);
 		mainFrame.setVisible(true);
 		
 		//Get real month/year
-		GregorianCalendar cal = new GregorianCalendar(); //Create calendar
-		realDay = cal.get(GregorianCalendar.DAY_OF_MONTH); //Get day
-		realMonth = cal.get(GregorianCalendar.MONTH); //Get month
-		realYear = cal.get(GregorianCalendar.YEAR); //Get year
+		GregorianCalendar calendar = new GregorianCalendar(); //Create calendar
+		realDay = calendar.get(GregorianCalendar.DAY_OF_MONTH); //Get day
+		realMonth = calendar.get(GregorianCalendar.MONTH); //Get month
+		realYear = calendar.get(GregorianCalendar.YEAR); //Get year
 		currentMonth = realMonth; //Match month and year
 		currentYear = realYear;
 		
 		//Add headers
-		String[] headers = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"}; //All headers
+		String[] headers = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun",}; //All headers
 		for (int i=0; i<7; i++){
 			modellofTableCalendar.addColumn(headers[i]);
 		}
@@ -94,8 +97,8 @@ public class MyDiary{
 		tableCalendar.getParent().setBackground(tableCalendar.getBackground()); //Set background
 
 		//No resize/reorder
-		tableCalendar.getTableHeader().setResizingAllowed(false);
-		tableCalendar.getTableHeader().setReorderingAllowed(false);
+		tableCalendar.getTableHeader().setResizingAllowed(true);
+		tableCalendar.getTableHeader().setReorderingAllowed(true);
 
 		//Single cell selection
 		tableCalendar.setColumnSelectionAllowed(true);
@@ -108,27 +111,52 @@ public class MyDiary{
 		modellofTableCalendar.setRowCount(6);
 		
 		//Populate table
-		for (int i=realYear-100; i<=realYear+100; i++){
+		for (int i=realYear-20; i<=realYear+80; i++){
 			comboBoxforChoosingYear.addItem(String.valueOf(i));
 		}
 		
 		//Refresh calendar
 		refreshCalendar (realMonth, realYear); //Refresh calendar
+		
+		
+		
+		
+		//EventsHandler event = new EventsHandler();
+		newEvent = new JLabel ("Add Information");
+		panelCalendar.add(newEvent);
+		newEvent.setBounds(360, 20, 200, 20);
+		
+		yearOfEvent = new JLabel ("Year:");
+		panelCalendar.add(yearOfEvent);
+		yearOfEvent.setBounds(320, 100, 60, 20);
+		
+		monthOfEvent = new JLabel ("Month:");
+		panelCalendar.add(monthOfEvent);
+		monthOfEvent.setBounds(320, 180, 200, 20);
+		
+		dayOfEvent = new JLabel ("Day:");
+		panelCalendar.add(dayOfEvent);
+		dayOfEvent.setBounds(320, 260, 200, 20);
+		
+		choiceOfYear = new Choice();
+		for (int i=realYear-20; i<=realYear+80; i++){
+			choiceOfYear.addItem(String.valueOf(i));
+		}
+		panelCalendar.add(choiceOfYear);
+		choiceOfYear.setBounds(400, 100, 80, 20);		
 	}
 	
 	public static void refreshCalendar(int month, int year){
-		//Variables
 		String[] months =  {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
-		int nod, som; //Number Of Days, Start Of Month
+		int numberOfDays, startOfMonth; 
 			
-		//Allow/disallow buttons
 		previousButton.setEnabled(true);
 		nextButton.setEnabled(true);
-		if (month == 0 && year <= realYear-10){previousButton.setEnabled(false);} //Too early
-		if (month == 11 && year >= realYear+100){nextButton.setEnabled(false);} //Too late
-		labelofMonth.setText(months[month]); //Refresh the month label (at the top)
-		labelofMonth.setBounds(160-labelofMonth.getPreferredSize().width/2, 25, 180, 25); //Re-align label with calendar
-		comboBoxforChoosingYear.setSelectedItem(String.valueOf(year)); //Select the correct year in the combo box
+		if (month == 0 && year <= realYear-10){previousButton.setEnabled(false);}			 	//Too early
+		if (month == 11 && year >= realYear+100){nextButton.setEnabled(false);} 				//Too late
+		labelofMonth.setText(months[month]); 													//Refresh the month label (at the top)
+		labelofMonth.setBounds(160-labelofMonth.getPreferredSize().width/2, 25, 180, 25); 		//Re-align label with calendar
+		comboBoxforChoosingYear.setSelectedItem(String.valueOf(year)); 							//Select the correct year in the combo box
 		
 		//Clear table
 		for (int i=0; i<6; i++){
@@ -138,28 +166,28 @@ public class MyDiary{
 		}
 		
 		//Get first day of month and number of days
-		GregorianCalendar cal = new GregorianCalendar(year, month, 1);
-		nod = cal.getActualMaximum(GregorianCalendar.DAY_OF_MONTH);
-		som = cal.get(GregorianCalendar.DAY_OF_WEEK);
+		GregorianCalendar calendar = new GregorianCalendar(year, month, 0);
+		numberOfDays = calendar.getActualMaximum(GregorianCalendar.DAY_OF_MONTH);
+		startOfMonth = calendar.get(GregorianCalendar.DAY_OF_WEEK);
 		
 		//Draw calendar
-		for (int i=1; i<=nod; i++){
-			int row = new Integer((i+som-2)/7);
-			int column  =  (i+som-2)%7;
+		for (int i=1; i<=numberOfDays; i++){
+			int row = new Integer((i+startOfMonth-2)/7);
+			int column  =  (i+startOfMonth-2)%7;
 			modellofTableCalendar.setValueAt(i, row, column);
 		}
 
-		//Apply renderers
-		tableCalendar.setDefaultRenderer(tableCalendar.getColumnClass(0), new tblCalendarRenderer());
+		//Apply renderer
+		tableCalendar.setDefaultRenderer(tableCalendar.getColumnClass(0), new tableCalendarRenderer());
 	}
 
-	static class tblCalendarRenderer extends DefaultTableCellRenderer{
+	static class tableCalendarRenderer extends DefaultTableCellRenderer{
 		public Component getTableCellRendererComponent (JTable table, Object value, boolean selected, boolean focused, int row, int column){
 			super.getTableCellRendererComponent(table, value, selected, focused, row, column);
-			if (column == 0 || column == 6){ //Week-end
+			if (column == 5 || column == 6){ 																				//Week-end
 				setBackground(new Color(255, 220, 220));
 			}
-			else{ //Week
+			else{ 																											//Week
 				setBackground(new Color(255, 255, 255));
 			}
 			if (value != null){
@@ -173,31 +201,33 @@ public class MyDiary{
 		}
 	}
 
-	static class btnPrev_Action implements ActionListener{
+	static class PreviousButtonAction implements ActionListener{
 		public void actionPerformed (ActionEvent e){
-			if (currentMonth == 0){ //Back one year
+			if (currentMonth == 0){ 					//Back one year
 				currentMonth = 11;
 				currentYear -= 1;
 			}
-			else{ //Back one month
+			else{ 										//Back one month
 				currentMonth -= 1;
 			}
 			refreshCalendar(currentMonth, currentYear);
 		}
 	}
-	static class btnNext_Action implements ActionListener{
+	
+	static class NextButtonAction implements ActionListener{
 		public void actionPerformed (ActionEvent e){
-			if (currentMonth == 11){ //Foward one year
+			if (currentMonth == 11){ 					//Forward one year
 				currentMonth = 0;
 				currentYear += 1;
 			}
-			else{ //Foward one month
+			else{ 										//Forward one month
 				currentMonth += 1;
 			}
 			refreshCalendar(currentMonth, currentYear);
 		}
 	}
-	static class cmbYear_Action implements ActionListener{
+	
+	static class ComboBoxYearAction implements ActionListener{
 		public void actionPerformed (ActionEvent e){
 			if (comboBoxforChoosingYear.getSelectedItem() != null){
 				String b = comboBoxforChoosingYear.getSelectedItem().toString();
